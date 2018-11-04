f04d7581a8ae5505ca084249539af7252f5861f7
mk668a <msftoaa@gmail.com>
2018-11-04 09:27:19 +0900

    new

---
+++ src/components/Camera.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="Camera">
+  <video autoplay playsinline muted id="webcam" width="250" height="250"></video>
+</div>
+</template>
+
+<script>
+import {
+  Webcam
+} from '../assets/webcam'
+
+export default {
+  name: "Camera",
+  data: function() {
+    return {
+      webcam: null,
+    }
+  },
+  mounted: function() {
+    this.loadWebcam();
+  },
+  methods: {
+    loadWebcam: function() {
+      this.webcam = new Webcam(document.getElementById('webcam'));
+      this.webcam.setup();
+    }
+  }
+};
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="main">
+  <h1>Change Face</h1>
+  <FaceModel></FaceModel>
+  <FaceAwareness></FaceAwareness>
+  <a>3D model face is changed by your facial expression</a>
+</div>
+</template>
+
+<script>
+import FaceModel from './FaceModel.vue'
+import FaceAwareness from './FaceAwareness.vue'
+
+export default {
+  name: 'Main',
+  data() {
+    return {}
+  },
+  components: {
+    FaceModel,
+    FaceAwareness
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1,
+h2 {
+  font-weight: bold;
+}
+
+.main{
+  margin: 10vh 10vw;
+}
+</style>
